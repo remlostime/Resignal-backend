7de834aaef0794da4fd2954086074fb0c9c05f07
Kai Chen <remlostime@gmail.com>
2026-01-24 19:27:22 -0800

    fix

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import fastify, { FastifyInstance } from 'fastify';
+import fastify, { type FastifyInstance } from 'fastify';
 
 const createServer = (): FastifyInstance => {
   const server = fastify({
